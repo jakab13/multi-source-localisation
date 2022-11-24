65843b678b70e833ac126f114d577eba92f4e5ed
pfriedrich-hub <52865128+pfriedrich-hub@users.noreply.github.com>
2022-11-24 10:03:20 +0100

    MSL example.

--- py/run_experiment.py
+++ py/run_experiment.py
@@ -7,15 +7,16 @@ import slab
 freefield.initialize(setup="dome", device=['RX81', 'RX8', 'E:\projects\multi-source-localisation\data\play_buf_msl.rcx'])
 # freefield.write(tag='bitmask', value=1, processors='RX81')
 
-vowels = ["a", "e", "i", "o", "u"]
+vowels = ["a", "e", "i", "o"]
 speakers_list = list(x for x in range(20, 27, 2))
+signal = slab.Sound.vowel(duration=10.0)
 freefield.write(tag="playbuflen", value=signal.n_samples, processors="RX81")
 
 for vowel, speaker in enumerate(speakers_list):
     speaker = freefield.pick_speakers(picks=speaker)[0]
     signal = slab.Sound.vowel(vowels[vowel], duration=10.0)
-    freefield.write(tag=f"data{vowel+1}", value=signal.data, processors=speaker.analog_proc)
-    freefield.write(tag=f"chan{vowel+1}", value=speaker.analog_channel, processors=speaker.analog_proc)
+    freefield.write(tag=f"data{vowel}", value=signal.data, processors=speaker.analog_proc)
+    freefield.write(tag=f"chan{vowel}", value=speaker.analog_channel, processors=speaker.analog_proc)
 
 
 
