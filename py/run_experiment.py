43c57e1f8ff27a0a0d7679be9e67b3ce1a6080c7
pfriedrich-hub <52865128+pfriedrich-hub@users.noreply.github.com>
2022-11-25 09:23:51 +0100

    excerpt from staircase branch

--- py/run_experiment.py
+++ py/run_experiment.py
@@ -1,11 +1,23 @@
 import freefield
 import slab
+from pathlib import Path
+import os
 
 # TODO: set unused channels on processors to 99.
 
 
 freefield.initialize(setup="dome", device=['RX81', 'RX8', 'E:\projects\multi-source-localisation\data\play_buf_msl.rcx'])
 # freefield.write(tag='bitmask', value=1, processors='RX81')
+
+speaker_list = list(x for x in range(20, 27) if x is not 23)
+sound_list = list()
+filepath = Path("E:\projects\multi-source-localisation\data\max")
+target_speaker = freefield.pick_speakers(picks=23)[0]
+
+
+for file in os.listdir(filepath):
+    sound_list.append(slab.Sound.read(filepath/file))
+
 
 vowels = ["a", "e", "i", "o"]
 speakers_list = list(x for x in range(20, 27, 2))
