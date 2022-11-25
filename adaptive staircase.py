fb21c26fe86e0b812479273701fa7a3683491065
pfriedrich-hub <52865128+pfriedrich-hub@users.noreply.github.com>
2022-11-25 14:53:07 +0100

    Update adaptive staircase.py
    Added button press (WIP)

--- adaptive staircase.py
+++ adaptive staircase.py
@@ -1,10 +1,11 @@
 import adstfunc as func
 import slab
-from pathlib import Path
+import pathlib
 import os
 import freefield
 import random
 import time
+import numpy as np
 
 
 # initialization + checking for existing ID
@@ -30,32 +31,38 @@ print(trials)  #for debugging
 # generate sound files
 gender = "M"  # F or M
 talker = "max"  # number of talker
-root = Path(os.getcwd())/"data"
+root = pathlib.Path(os.getcwd())/"data"
 if not os.path.exists(root):
     os.mkdir(root)
 duration = 2.0
 samplerate = 48828
 for number in list(range(1, 11)):
-    filename = Path(f"talker-{talker}_number-{number}_gender-{gender}.wav")
-    filepath = Path(root/f"{talker}")
+    filename = pathlib.Path(f"talker-{talker}_number-{number}_gender-{gender}.wav")
+    filepath = pathlib.Path(root/f"{talker}")
     input(f"Press any key to start recording number {number}")
     sound = slab.Sound.record(duration=duration, samplerate=samplerate)
     if not os.path.exists(filepath):
         os.mkdir(filepath)
     sound.write(filepath/filename)
-    print(f"Sucessfully saved sound {number} from talker {talker}!")
+    print(f"Successfully saved sound {number} from talker {talker}!")
 # 1. locate sound files
 # 2. load sound files
 
 
 #TODO
 # test for loudness threshold and write a quick savefile
+DIR = pathlib.Path(os.getcwd()).absolute()
+proc_list = [['RP2', 'RP2', DIR / 'data' / "rcx" / 'button_rec.rcx'],
+             ['RX81', 'RX8', DIR / 'data' / "rcx" / 'play_buf_msl.rcx'],
+             ['RX82', 'RX8', DIR / 'data' / "rcx" / 'play_buf_msl.rcx']]
+freefield.initialize(setup="dome", device=proc_list)  # initialize freefield
+
+
 masker_sound = slab.Sound.pinknoise(duration=2.0)  # masker sound is always noise
-stairs = slab.Staircase(start_val=70, n_reversals=5, step_sizes=[4, 1])  # staircase for different target dB levels
-freefield.initialize(setup="dome", device=["RX81", "RX8", "E:/projects/multi-source-localisation/data/rcx/play_buf_msl.rcx"])  # initialize freefield
+stairs = slab.Staircase(start_val=80, n_reversals=5, step_sizes=[4, 1])  # staircase for different target dB levels
 speaker_list = list(x for x in range(20, 27) if x != 23)  # central dome speakers without speaker 23 (ele:0°)
 sound_list = list()  # list of sounds to choose target from (numbers 1-10)
-filepath = Path(os.getcwd()) / "data" / "max"  # example file path
+filepath = pathlib.Path(os.getcwd()) / "data" / "max"  # example file path
 target_speaker = freefield.pick_speakers(picks=23)[0]  # pick central speaker
 
 for file in os.listdir(filepath):  # load sound files into list
@@ -66,14 +73,29 @@ freefield.write(tag="chan0", value=target_speaker.analog_channel, processors=tar
 freefield.write(tag="data1", value=masker_sound.data, processors=target_speaker.analog_proc)  # masker sound
 freefield.write(tag="playbuflen", value=masker_sound.n_samples, processors="RX81")  # 2 seconds duration
 
-
+responses = dict()
 for trial in list(range(1, 10)):  # 10 trials
-    target_sound = random.choice(sound_list)  # choose random number from sound_list
+    target_i = random.choice(range(len(sound_list)))
+    target_sound = sound_list[target_i]  # choose random number from sound_list
+    masker_sound = slab.Sound.pinknoise(duration=2.0)  # masker sound is always noise
+    masker_sound.level = random.choice(range(60, 80))
+    freefield.write(tag="data1", value=masker_sound.data, processors=target_speaker.analog_proc)  # masker sound
+    target_sound.level = random.choice(range(60, 80))
     masker_speaker = freefield.pick_speakers(picks=random.choice(speaker_list))[0]  # pick random masker speaker
     freefield.write(tag="data0", value=target_sound.data, processors=target_speaker.analog_proc)  # load target sound
     freefield.write(tag="chan1", value=masker_speaker.analog_channel, processors=masker_speaker.analog_proc)  # load masker speaker
     freefield.play()  # play trial
-    time.sleep(2.0)  # wait a bit so that trials do not overlap (not needed when button press is implemented)
+    while freefield.read(tag="playback", processor="RX81"):  # wait until stimuli have been presented
+        time.sleep(0.1)
+    curr_response = int(freefield.read(tag="response", processor="RP2"))
+    if curr_response != 0:
+        response = int(np.log2(curr_response)) + 1
+        if response == 5:
+            response = 0
+    responses["actual"] = target_i + 1
+    responses["perceived"] = curr_response
+    while freefield.read(tag="playback", n_samples=1, processor="RP2"):
+        time.sleep(0.01)
 
 freefield.halt()
 
