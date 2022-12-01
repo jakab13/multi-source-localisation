db8819afbe7ce6b52ce69d4146e219bc42763d60
Max Schulz <max.schulz10@web.de>
2022-12-01 11:44:55 +0100

    Update run_experiment.py

--- py/record_stims.py
+++ py/record_stims.py
@@ -26,7 +26,7 @@ for number in range(nums_rec):  # record sound files
 # Alternatively, use google TTS API to generate stimuli.
 
 gender = "F"  # F or M
-talker = "gTTS"  # number of talker
+talker = "gTTS-de"  # number of talker
 nums_rec = 5
 root = pathlib.Path.cwd() / "data" / "sounds"
 language = "de"
@@ -35,10 +35,15 @@ if not os.path.exists(root):
     os.mkdir(root)
 
 for number in range(1, nums_rec+1):  # record sound files
-    filename = pathlib.Path(f"talker-{talker}_number-{number}_gender-{gender}.wav")
+    filename = pathlib.Path(f"talker-{talker}_number-{number}_gender-{gender}")
     filepath = pathlib.Path(root/f"{talker}")
     if not os.path.exists(filepath):
         os.mkdir(filepath)
     tts = gtts.gTTS(str(number), lang=language)
     tts.save(str(filepath/filename) + ".wav")
     print(f"Successfully saved sound {number} from talker {talker}!")
+
+fp = pathlib.Path("D:\Projects\multi-source-localisation\data\sounds\gTTS-en")
+for file in os.listdir(fp):
+    sound = slab.Sound.read(fp/file)
+    sound.write(fp/file/"_new.wav")
