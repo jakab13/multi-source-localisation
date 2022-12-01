3520362d45f0f30a5b1f9510602708a70dda61dc
pfriedrich-hub <61830224+mxschlz@users.noreply.github.com>
2022-12-01 15:05:04 +0100

    update WIP

--- py/run_experiment.py
+++ py/run_experiment.py
@@ -1,54 +1,62 @@
 import freefield
 import slab
-from pathlib import Path
+import pathlib
 import os
 import random
 import time
+import numpy as np
 
 # TODO: set unused channels on processors to 99.
 
-
-freefield.initialize(setup="dome", device=['RX81', 'RX8', 'E:\projects\multi-source-localisation\data\\rcx\play_buf_msl.rcx'])
-filepath = Path("E:\projects\multi-source-localisation\data/sounds/gTTS-de")
-# freefield.write(tag='bitmask', value=1, processors='RX81')
-
+# initialize FF
+DIR = pathlib.Path(os.getcwd()).absolute()
+proc_list = [['RP2', 'RP2', DIR / 'data' / "rcx" / 'button_rec.rcx'],
+             ['RX81', 'RX8', DIR / 'data' / "rcx" / 'play_buf_msl.rcx'],
+             ['RX82', 'RX8', DIR / 'data' / "rcx" / 'play_buf_msl.rcx']]
+freefield.initialize(setup="dome", device=proc_list)  # initialize freefield
+# pick speakers and sounds
 speaker_list = list(x for x in range(20, 27))
 sound_list = list()
-samplerate = 48828 * 2
-play_duration = 2.0
-
+stim_dur = 10  # can be 0.3,1,5 or 10s
+filepath = pathlib.Path(f"E:\projects\multi-source-localisation\data/sounds/{stim_dur}s")
 for file in os.listdir(filepath):
     sound = slab.Sound.read(filepath/file)
     # sound.data = sound.data[::-1]
     sound_list.append(sound)
 
+# number of maximum talkers
+talker_count = list(x for x in range(1, 6))  # number of maximum talkers
+freefield.write(tag="playbuflen", value=sound_list[0].n_samples, processors="RX81")
 
+# initialize sequence and response object
+seq = slab.Trialsequence(conditions=4, n_reps=5)
+results = slab.ResultsFile()
 
-speaker_list = list(x for x in range(20, 27))
-talker_count = list(x for x in range(1, 6))
-trials = 20
-freefield.write(tag="playbuflen", value=samplerate, processors="RX81")
-
-while not freefield.read(tag="response", processor="RP2"):
-    time.sleep(0.01)
-curr_response = int(freefield.read(tag="response", processor="RP2"))
-if curr_response != 0:
-    reaction_time = int(round(time.time() - start_time, 3) * 1000)
-    response = int(np.log2(curr_response))
-
-for trial in range(trials):
+# loop through sequence
+for trial in seq:
+    seq.__next__()
     num_talkers = random.choice(talker_count)
     speaker_ids = random.sample(speaker_list, num_talkers)
     sound_list_copy = sound_list.copy()
+    response = None
     for i, speaker_id in enumerate(speaker_ids):
         speaker = freefield.pick_speakers(picks=speaker_id)[0]
         signal = random.choice(sound_list_copy)
         sound_list_copy.remove(signal)
-        freefield.write(tag=f"data{i}", value=signal.data.flatten(), processors=speaker.analog_proc)
+        freefield.write(tag=f"data{i}", value=signal.data, processors=speaker.analog_proc)
         freefield.write(tag=f"chan{i}", value=speaker.analog_channel, processors=speaker.analog_proc)
+    start_time = time.time()
     freefield.play()
-    while freefield.read(tag="playback", n_samples=1, processor=speaker.analog_proc):
+    freefield.wait_to_finish_playing()
+    while not freefield.read(tag="response", processor="RP2"):
         time.sleep(0.01)
+    curr_response = int(freefield.read(tag="response", processor="RP2"))
+    if curr_response != 0:
+        reaction_time = int(round(time.time() - start_time, 3) * 1000)
+        response = int(np.log2(curr_response))
+    results.write(response, "response")
+    results.write(num_talkers, "solution")
+    results.write(reaction_time, "rc")
 
-freefield.play()
+
 freefield.halt()
