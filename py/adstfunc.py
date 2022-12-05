7ec5dba1fc356f09bd60f020b8f8bae1589cfa90
Netzra <108935999+Netzra@users.noreply.github.com>
2022-12-05 19:48:15 +0100

    Progressupdate

--- py/adstfunc.py
+++ py/adstfunc.py
@@ -37,6 +37,54 @@ def init(projectName):
                             )
         writer.writerow(['participant_ID', 'age', 'sex', 'Hearing_related'])
     print("set up finished")
+    
+# not yet functioning
+# adding participant to file
+def adding(name, projectName, age, sex, hearing="", currentSession = 1):
+
+    vorhanden = False
+    count = 0
+
+    #failsafe
+    if not os.listdir().__contains__(projectName):
+        print("given Projectname doesnt exist")
+        exit(1)
+    try:
+        int(age)
+    except ValueError:
+        print("pls enter an proper age. For example: 23")
+        exit(2)
+
+    with open("mastersheet.txt", "r") as master:
+        for line in master:
+            if line.__contains__(name):
+                print ("found: "+line)
+                count =+ 1
+
+    if count == 0:
+        #TODO
+        # point of breaking -> while writing this function seperated from adstfunc.py
+        # if later on added to adsfunc change adstfunc.randId() to just randId()
+        
+
+        id = adstfunc.randId()
+        with open("mastersheet.txt", "w") as file:
+            file.write(name + " "+id)
+
+        with open("particpants.csv", "w", newline='') as csv:
+            writer = csv.writer(csv,
+                                delimiter=',',
+                                quotechar='|',
+                                quoting=csv.QUOTE_MINIMAL
+                                )
+
+            writer.writerow([id, age, sex, hearing])
+
+    if count == 1:
+        with open("mastersheet.txt", "r") as r:
+            for line in r:
+                if name == line.split(' ')[1]:
+                    id = line.split(' ')[2]
 
 if __name__ == "__main__":
     init("MSL")
