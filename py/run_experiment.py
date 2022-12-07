37f2ed5e0080c97db39272945d9d51267e87dd16
pfriedrich-hub <61830224+mxschlz@users.noreply.github.com>
2022-12-07 18:03:04 +0100

    Experiment with numbers (last exploitation of num-judge branch)

--- py/run_experiment.py
+++ py/run_experiment.py
@@ -1,27 +1,79 @@
 import freefield
 import slab
+import pathlib
+import os
+import random
+import time
+import numpy as np
+# import head_tracking.meta_motion.mm_pose as motion_sensor
+
+fs = 48828
+slab.set_default_samplerate(fs)
 
 # TODO: set unused channels on processors to 99.
 
+# initialize FF
+DIR = pathlib.Path(os.getcwd()).absolute()
+proc_list = [['RP2', 'RP2', DIR / 'data' / "rcx" / 'button_rec.rcx'],
+             ['RX81', 'RX8', DIR / 'data' / "rcx" / 'play_buf_msl.rcx'],
+             ['RX82', 'RX8', DIR / 'data' / "rcx" / 'play_buf_msl.rcx']]
+freefield.initialize(setup="dome", device=proc_list)  # initialize freefield
 
-freefield.initialize(setup="dome", device=['RX81', 'RX8', 'E:\projects\multi-source-localisation\data\play_buf_msl.rcx'])
-# freefield.write(tag='bitmask', value=1, processors='RX81')
+# pick speakers and sounds
+speaker_list = list(x for x in range(20, 27))
+stim_dur = 10  # can be 0.3,1,5 or 10s
+filepath = pathlib.Path("C:\\Users\\neurobio\\Desktop\\sounds_resampled\\tts-numbers\\10s")
+sound_list = slab.Precomputed(slab.Sound.read(filepath/file) for file in os.listdir(filepath))
 
-vowels = ["a", "e", "i", "o"]
-speakers_list = list(x for x in range(20, 27, 2))
-signal = slab.Sound.vowel(duration=10.0)
-freefield.write(tag="playbuflen", value=signal.n_samples, processors="RX81")
+# set playbuflen tag
+freefield.write(tag="playbuflen", value=750000, processors="RX81")
 
-for vowel, speaker in enumerate(speakers_list):
-    speaker = freefield.pick_speakers(picks=speaker)[0]
-    signal = slab.Sound.vowel(vowels[vowel], duration=10.0)
-    freefield.write(tag=f"data{vowel}", value=signal.data, processors=speaker.analog_proc)
-    freefield.write(tag=f"chan{vowel}", value=speaker.analog_channel, processors=speaker.analog_proc)
+# initialize sequence and response object
+seq = slab.Trialsequence(conditions=3, n_reps=5)
+results = slab.ResultsFile()
+
+# loop through sequence
+for trial in seq:
+    speaker_ids = random.sample(speaker_list, trial)
+    response = None
+    for i, speaker_id in enumerate(speaker_ids):
+        speaker = freefield.pick_speakers(picks=speaker_id)[0]
+        signal = random.choice(sound_list)
+        sound_list.remove(signal)
+        freefield.write(tag=f"data{i}", value=signal.data, processors=speaker.analog_proc)
+        freefield.write(tag=f"chan{i}", value=speaker.analog_channel, processors=speaker.analog_proc)
+    start_time = time.time()
+    #sensor = motion_sensor.start_sensor()
+    #offset = motion_sensor.calibrate_pose(sensor)
+    #pose = motion_sensor.get_pose(sensor, 30)  # set initial isi based on pose-target difference
+    #response = 0
+    # not response:
+        #pose = motion_sensor.get_pose(sensor, 30)  # set initial isi based on pose-target difference
+        #if all(pose):
+            #pose = pose - offset
+            #print('head pose: azimuth: %.1f, elevation: %.1f' % (pose[0], pose[1]), end="\r", flush=True)
+        #else:
+           # print('no head pose detected', end="\r", flush=True)
+       # response = freefield.read('response', processor='RP2')
+    #if all(pose):
+        #print('Response| azimuth: %.1f, elevation: %.1f' % (pose[0], pose[1]))
+    freefield.play()
+    freefield.wait_to_finish_playing()
+    while not freefield.read(tag="response", processor="RP2"):
+        time.sleep(0.01)
+    curr_response = int(freefield.read(tag="response", processor="RP2"))
+    if curr_response != 0:
+        reaction_time = int(round(time.time() - start_time, 3) * 1000)
+        response = int(np.log2(curr_response))
+    solution = trial + 1
+    correct_response = True if solution / response == 1 else False
+    results.write(response, "response")
+    results.write(solution, "solution")
+    results.write(reaction_time, "rc")
+    results.write(correct_response, "is_correct")
+    while freefield.read(tag="playback", n_samples=1, processor="RP2"):
+        time.sleep(0.01)
 
 
-
-freefield.play()
+motion_sensor.disconnect(sensor)
 freefield.halt()
-
-for i, speaker in enumerate(speakers):
-    freefield.write()
