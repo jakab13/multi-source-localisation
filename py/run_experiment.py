2f9ec8896cf453486e3be4a68d6a61a3565c5ffb
pfriedrich-hub <61830224+mxschlz@users.noreply.github.com>
2022-12-07 12:38:35 +0100

    Update run_experiment.py
    Code for head pose tracking (NOT WORKING)

--- py/run_experiment.py
+++ py/run_experiment.py
@@ -5,6 +5,10 @@ import os
 import random
 import time
 import numpy as np
+import head_tracking.meta_motion.mm_pose as motion_sensor
+
+fs = 48828
+slab.set_default_samplerate(fs)
 
 # TODO: set unused channels on processors to 99.
 
@@ -18,29 +22,41 @@ freefield.initialize(setup="dome", device=proc_list)  # initialize freefield
 # pick speakers and sounds
 speaker_list = list(x for x in range(20, 27))
 stim_dur = 1  # can be 0.3,1,5 or 10s
-filepath = pathlib.Path(DIR / f"data/sounds/{stim_dur}s")
+filepath = pathlib.Path(DIR / f"data/sounds/tts-numbers/{stim_dur}s")
 sound_list = slab.Precomputed(slab.Sound.read(filepath/file) for file in os.listdir(filepath))
 
-# number of maximum talkers
-freefield.write(tag="playbuflen", value=100000, processors="RX81")
+# set playbuflen tag
+freefield.write(tag="playbuflen", value=60000, processors="RX81")
 
 # initialize sequence and response object
-seq = slab.Trialsequence(conditions=4, n_reps=5)
+seq = slab.Trialsequence(conditions=3, n_reps=5)
 results = slab.ResultsFile()
 
 # loop through sequence
 for trial in seq:
     speaker_ids = random.sample(speaker_list, trial)
-    sound_list_copy = sound_list.copy()
     response = None
     for i, speaker_id in enumerate(speaker_ids):
         speaker = freefield.pick_speakers(picks=speaker_id)[0]
-        signal = random.choice(sound_list_copy)
-        signal.data = signal.data[::-1]
-        sound_list_copy.remove(signal)
+        signal = random.choice(sound_list)
+        sound_list.remove(signal)
         freefield.write(tag=f"data{i}", value=signal.data, processors=speaker.analog_proc)
         freefield.write(tag=f"chan{i}", value=speaker.analog_channel, processors=speaker.analog_proc)
     start_time = time.time()
+    #sensor = motion_sensor.start_sensor()
+    #offset = motion_sensor.calibrate_pose(sensor)
+    #pose = motion_sensor.get_pose(sensor, 30)  # set initial isi based on pose-target difference
+    #response = 0
+    # not response:
+        #pose = motion_sensor.get_pose(sensor, 30)  # set initial isi based on pose-target difference
+        #if all(pose):
+            #pose = pose - offset
+            #print('head pose: azimuth: %.1f, elevation: %.1f' % (pose[0], pose[1]), end="\r", flush=True)
+        #else:
+           # print('no head pose detected', end="\r", flush=True)
+       # response = freefield.read('response', processor='RP2')
+    #if all(pose):
+        #print('Response| azimuth: %.1f, elevation: %.1f' % (pose[0], pose[1]))
     freefield.play()
     freefield.wait_to_finish_playing()
     while not freefield.read(tag="response", processor="RP2"):
@@ -59,4 +75,5 @@ for trial in seq:
         time.sleep(0.01)
 
 
+motion_sensor.disconnect(sensor)
 freefield.halt()
