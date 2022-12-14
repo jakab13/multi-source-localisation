1e6e70cd68dd025571820f86fb900336a97c0e40
Max Schulz <max.schulz10@web.de>
2022-12-14 18:55:46 +0100

    removal of pretrial headpose estimation

--- experiment/run_experiment.py
+++ experiment/run_experiment.py
@@ -6,8 +6,7 @@ import random
 import time
 import numpy as np
 from meta_motion import mm_pose as motion_sensor
-import from meta_motion import scan_connect
-
+from meta_motion import scan_connect
 
 # initialize FF
 DIR = pathlib.Path(os.getcwd()).absolute()
@@ -18,49 +17,54 @@ freefield.initialize(setup="dome", device=proc_list)  # initialize freefield
 
 # pick speakers and sounds
 speaker_list = list(x for x in range(20, 27))
-filepath = pathlib.Path("E:\\projects\\multi-source-localisation\\data\\sounds")
+filepath = pathlib.Path("E:\\projects\\multi-source-localisation\\data\\sounds\\tts-numbers_resamp")
 sound_list = slab.Precomputed(slab.Sound.read(filepath/file) for file in os.listdir(filepath))
 starttone = slab.Sound.read(DIR / "data" / "sounds" / "bell.wav")
-buzztone = slab.Sound.read(DIR / "data" / "sounds" / "buzztone.wav")
-samplerate = 24414
+buzztone = slab.Sound.read(DIR / "data" / "sounds" / "bell.wav")
+# samplerate = 24414
 
 # initialize sequence and response object
 seq = slab.Trialsequence(conditions=5, n_reps=3)
 results = slab.ResultsFile()
 
-# play starting tone and wait for button press
+# start headpose sensor
+sensor = motion_sensor.start_sensor()
+
+# play starting tone
 freefield.write(tag=f"data0", value=starttone.data, processors="RX81")
 freefield.write(tag=f"chan0", value=23, processors="RX81")
-freefield.write(tag="playbuflen", value=starttone.n_samples, processors="RX81")
 freefield.play()
 freefield.wait_to_finish_playing()
 freefield.wait_for_button()
 
-# run experiment after button press
+# run experiment
 for trial in seq:
+    offset = motion_sensor.calibrate_pose(sensor)
+    tracking = True
+    while tracking:
+        pose = motion_sensor.get_pose(sensor, 30)  # set initial isi based on pose-target difference
+        if all(pose):
+            for x, y in zip(pose, offset):
+                if np.abs(x - y) > 5:
+                    freefield.write(tag=f"data0", value=buzztone.data, processors="RX81")
+                    freefield.write(tag=f"chan0", value=23, processors="RX81")
+                    freefield.play()
+                    freefield.wait_to_finish_playing()
+                    freefield.wait_for_button()
+            pose = pose - offset
+            print('head pose at azimuth: %.1f, elevation: %.1f' % (pose[0], pose[1]), end="\r", flush=True)
+            tracking = False if freefield.read('response', processor='RP2') is True else False
+        else:
+           print('no head pose detected', end="\r", flush=True)
     speaker_ids = random.sample(speaker_list, trial)
     response = None
     for i, speaker_id in enumerate(speaker_ids):
         speaker = freefield.pick_speakers(picks=speaker_id)[0]
         signal = random.choice(sound_list)
-        # sound_list.remove(signal)
+        freefield.write(tag="playbuflen", value=signal.n_samples, processors=["RX81", "RX82"])  # set playbuflen tag
         freefield.write(tag=f"data{i}", value=signal.data, processors=speaker.analog_proc)
         freefield.write(tag=f"chan{i}", value=speaker.analog_channel, processors=speaker.analog_proc)
     start_time = time.time()
-    sensor = motion_sensor.start_sensor()
-    offset = motion_sensor.calibrate_pose(sensor)
-    pose = motion_sensor.get_pose(sensor, 30)  # set initial isi based on pose-target difference
-    response = 0
-    while not response:
-        pose = motion_sensor.get_pose(sensor, 30)  # set initial isi based on pose-target difference
-        if all(pose):
-            pose = pose - offset
-            print('head pose: azimuth: %.1f, elevation: %.1f' % (pose[0], pose[1]), end="\r", flush=True)
-        else:
-            print('no head pose detected', end="\r", flush=True)
-        response = freefield.read('response', processor='RP2')
-    if all(pose):
-        print('Response| azimuth: %.1f, elevation: %.1f' % (pose[0], pose[1]))
     freefield.play()
     freefield.wait_to_finish_playing()
     freefield.wait_for_button()
