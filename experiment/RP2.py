c2850dca8c2130a754c4edb620bad9731d1eaf56
Max Schulz <max.schulz10@web.de>
2022-12-16 09:37:56 +0100

    Implemented wait_for_button()

--- experiment/RP2.py
+++ experiment/RP2.py
@@ -2,25 +2,24 @@ from labplatform.config import get_config
 from labplatform.core.Device import Device
 from labplatform.core.Setting import DeviceSetting
 from labplatform.core import TDTblackbox as tdt
-
+import time
 from traits.api import CFloat, CInt, Str, Any, Instance
 import os
-
 import logging
 
 log = logging.getLogger(__name__)
 
-# TODO: What about the device connection?
+# TODO: What about the device connection? GB OR USB?
 class RP2_Setting(DeviceSetting):
     sampling_freq = CFloat(48288.125, group='primary', dsec='sampling frequency of the device (Hz)')
     buffer_size_max = CInt(50000, group='status', dsec='buffer size cannot be larger than this')
     file = Str('RCX\\button_rec.rcx', group='primary', dsec='name of the rcx file to load')
     processor = Str('RP2', group='status', dsec='name of the processor')
-    connection = Str('USB', group='status', dsec='')
+    connection = Str('GB', group='status', dsec='')
     index = CInt(1, group='primary', dsec='index of the device to connect to')
     stimulus = Any(group='primary', dsec='stimulus to play', reinit=False)
 
-class RP2(Device):
+class RP2_Device(Device):
     setting = RP2_Setting()
     handle = Any
 
@@ -34,10 +33,10 @@ class RP2(Device):
     def _configure(self, **kargs):
         if self.setting.stimulus.__len__():
             # self.handle.WriteTagV('datain', 0, self.setting.stimulus)
-            self.handle.SetTagVal('playbuflen', len(self.setting.stimulus))
+            self.handle.write('playbuflen', len(self.setting.stimulus))
 
     def _start(self):
-        self.handle.SoftTrg(1)
+        print("Running RP2 ... ")
 
     def _pause(self):
         pass
@@ -45,20 +44,19 @@ class RP2(Device):
     def _stop(self):
         self.handle.Halt()
 
-    def thread_func(self):
-        while self.handle.GetTagVal('playback'):
-            pass
+    def wait_for_button(self):
+        while not self.handle.read(tag="response", proc=self.settings.processor):
+            time.sleep(0.1)
 
-        if self.experiment:
-            self.pause()
-            self.experiment.process_event({'trial_stop': 0})
+
 
 
 if __name__ == "__main__":
-    import numpy as np
-
     RP2 = RP2()
-    RP2.setting.stimulus = np.ones(int(RP2.setting.sampling_freq * 1))
     RP2.initialize()
     RP2.configure()
     RP2.start()
+    RP2.wait_for_button()
+    RP2.pause()
+    RP2.stop()
+    RP2.pause()
