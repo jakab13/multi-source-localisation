9b1439c97262b2f1af0056f7f674a46b7b9b90a8
Max Schulz <max.schulz10@web.de>
2022-12-18 21:03:25 +0100

    headpose estimation example

--- headpose_estimation/headpose_example.py
+++ headpose_estimation/headpose_example.py
@@ -9,7 +9,8 @@ for i in range(cv2.CAP_PROP_FRAME_COUNT):
     cam.grab()
 ret, image = cam.retrieve()
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cam.release()
 
 est.detect_landmarks(image, plot=True)  # plot the result of landmark detection
 roll, pitch, yaw = est.pose_from_image(image)  # estimate the head pose
+
+cam.release()
