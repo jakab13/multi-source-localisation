6ce19574ea40a627d6ba620c1faa7967a6e70ede
Max Schulz <max.schulz10@web.de>
2023-01-07 11:51:52 +0100

    fixed get_config()

--- experiment/setup_exp_settings.py
+++ experiment/setup_exp_settings.py
@@ -1,15 +1,20 @@
 from labplatform.config import *
 
 # TODO: get_config does not load custom config settings
-# load default settings
-_settings, __changed_settings = load_settings()
-
-set_config("EXPERIMENTER", ["Jakab, Nathalie, Carsten, Max"])
-set_config("PARADIGM", ["Numerosity_Judgement", "Spatial_Unmasking"])
-
-save_env_config("MSL")
 
 if __name__ == "__main__":
-    DIR = _settings["SETTINGS_ROOT"]
-    settings = load_settings(DIR)
-    setting = get_config()
+    # load default settings
+    _settings, __changed_settings = load_settings()
+
+    # change config params
+    set_config("EXPERIMENTER", ["Jakab, Nathalie, Carsten, Max"])
+    set_config("PARADIGM", ["Numerosity_Judgement", "Spatial_Unmasking"])
+
+    # save new config file
+    save_env_config("MSL")
+
+    # load custom config
+    DIR = get_config("SETTINGS_ROOT")
+    fp = os.path.join(DIR, os.listdir(DIR)[0])
+    settings, changed_settings = load_settings(fp)
+    setting = get_config(setting="PARADIGM")
