e270dfd17e65b7235a3cb4e893ebc4a00cd1b08d
Max Schulz <max.schulz10@web.de>
2023-01-10 09:15:03 +0100

    Aruco head pose estimation first implementation attempt

--- experiment/Camera.py
+++ experiment/Camera.py
@@ -10,11 +10,186 @@ import numpy as np
 from Speakers.speaker_config import SpeakerArray
 import os
 from simple_pyspin import Camera
+import cv2
+import PySpin
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 log = logging.getLogger(__name__)
 
-# TODO: Try camera calibration
+# TODO: Implement aruco headpose estimation
+
+class ArucoCamSetting(DeviceSetting):
+    """
+    Class for defining the camera settings. primary group parameters are supposed to be changed and sometimes
+    reinitialized, whereas status group parameters are not.
+    """
+    name = Str("FireFly", group="status", dsec="")
+    type = Str("image", group="status", dsec="nature of the signal")
+    dtype = Instance(np.uint8, group="status", dsec="data type")
+    shape = Tuple(1080, 1440, group="primary", dsec="default shape of the image", reinit=False)
+    root = Str(get_config(setting="BASE_DIRECTORY"), group="status", dsec="Labplatform root directory")
+    setup = Str("dome", group="status", dsec="experiment setup")
+    file = Str(f"{setup.default_value}_speakers.txt", group="status", dsec="Speaker file")
+    led = Any(Instance, group="primary", dsec="Central speaker with attached led for initial camera calibration",
+              reinit=False)
+    calibrated = Bool(group="primary", dsec="Tells whether camera is calibrated or not", reinit=False)
+    pose = List(group="primary", dsec="Headpose", reinit=False)
+    offset = Float(group="primary", dsec="Camera offset, estimated during camera calibration and subtracted from pose",
+                   reinit=False)
+
+class FlirCam(Device):
+    """
+    Class for controlling the device. We set the setting class as this classes attribute (.setting). Also we have
+    placeholders for the offset of the camera, the pose for each trial, the PoseEstimator (CNN), the cam itself and the
+    tdt handle, mainly for calibration.
+    """
+    setting = ArucoCamSetting()
+    cam = Any()
+    handle = Any()
+    aruco_dicts = [cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_100),
+                   cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_100)]
+    params = cv2.aruco.DetectorParameters_create()
+    system = PySpin.System.GetInstance()
+    cams = system.GetCameras()
+
+    def _initialize(self, **kwargs):
+        """
+        Initializes the device and sets the state to "created". Necessary before running the device.
+        """
+        print("Initializing cameras ... ")
+        spks = SpeakerArray(file=os.path.join(self.setting.root, self.setting.file))  # initialize speakers.
+        spks.load_speaker_table()  # load speakertable
+        self.setting.led = spks.pick_speakers(23)[0]  # pick central speaker
+        for cam in self.cams:  # initialize cameras
+            cam.Init()
+            cam.ExposureAuto.SetValue(PySpin.ExposureAuto_Off)  # disable auto exposure time
+            cam.ExposureTime.SetValue(10000.0)  # ???
+            try:
+                cam.BeginAcquisition()
+            except:
+                print('camera already streaming')
+
+    def _configure(self, **kwargs):
+        """
+        Manipulates the setting parameters of the class and sets the state to "ready. Needs to be called in each trial.
+        """
+        print("Configuring camera settings ...")
+        pass
+
+    def _start(self, **kwargs):
+        """
+        Runs the device and sets the state to "running". Here lie all the important steps the camera has to do in each
+        trial, such as acquiring and writing the head pose.
+        """
+        self.cam.start()  # start recording images into the camera buffer
+        print("Acquiring image ... ")
+        try:
+            if self.setting.calibrated:
+                img = self.cam.get_array()  # Get image as numpy array
+                roll, pitch, yaw = self.est.pose_from_image(img)  # estimate the head pose from the np array
+                if self.offset:
+                    self.pose.append([roll-self.offset[0], pitch-self.offset[1], yaw-self.offset[2]])  # subtract offset
+                else:
+                    print("WARNING: Camera not calibrated, head pose might be unreliable ...")
+                    self.pose.append([roll, pitch, yaw])
+                print("Acquired pose!")
+        except ValueError:
+            print("Could not recognize face, make sure that camera can see the face!")
+
+    def _pause(self, **kwargs):
+        """
+        Pauses the camera and sets the state to "paused".
+        """
+        print("Pausing cameras ... ")
+        for cam in self.cams:
+            if cam.IsInitialized():
+                cam.EndAcquisition()
+                cam.DeInit()
+
+    def _stop(self):
+        """
+        Closes the camera and cleans up and sets the state to "stopped".
+        """
+        print("Halting cameras ... ")
+        for cam in self.cams:
+            if cam.IsInitialized():
+                cam.EndAcquisition()
+                cam.DeInit()
+            del cam
+        self.cams.Clear()
+        self.system.ReleaseInstance()
+
+    def calibrate(self):
+        """
+        Calibrates the camera. Initializes the RX81 to access the central loudspeaker. Illuminates the led on ele, azi 0°,
+        then acquires the headpose and uses it as the offset. Turns the led off afterwards.
+        """
+        print("Calibrating camera ...")
+        expdir = get_config('DEVICE_ROOT')
+        self.handle = tdt.Processors()
+        self.handle.initialize(proc_list=[[self.setting.processor,
+                                           self.setting.processor,
+                                           os.path.join(expdir, self.setting.file)]],
+                                           connection=self.setting.connection)
+        print(f"Initialized {self.setting.processor}{self.setting.index}.")
+        self.handle.write(tag='bitmask',
+                          value=self.setting.led_spk.digital_channel,
+                          processors=self.setting.led_spk.digital_proc)  # illuminate LED
+        self.handle
+        try:
+            roll, pitch, yaw = self.get_pose()  # get head pose
+            self.offset = [roll, pitch, yaw]  # use head pose as offset
+            self.handle.write(tag='bitmask', value=0, processors=self.setting.led_spk.digital_proc)  # turn off LED
+            self.setting.calibrated = True
+            print("Successfully calibrated camera!")
+        except ValueError:
+            print("Could not see the face, make sure that the camera is seeing the face!")
+        if self.setting.calibrated:
+            self.handle.halt()
+
+    def snapshot(self, cmap="gray"):
+        """
+        Args:
+            cmap: matplotlib colormap
+        """
+        print("Acquiring snapshot ...")
+        image = self.cam.get_array()  # get image as np array
+        plt.imshow(image, cmap=cmap)  # show image
+        plt.show()
+
+    @staticmethod
+    def brighten(image, factor):
+        """
+        Brightens the image by a factor.
+        Args:
+            image: image to be brightened, must be in the form of an array.
+            factor: factor by which the image gets brightened.
+
+        Returns:
+            brightened image array.
+        """
+        img = Image.fromarray(image)  # transform array to PIL.Image object
+        filter = ImageEnhance.Brightness(img)  # create brightness filter
+        brightimg = filter.enhance(factor)  # enhance image
+        array = np.asarray(brightimg)  # transform back to array
+        return array
+
+    @staticmethod
+    def sharpen(image, factor):
+        """
+        Args:
+            image: image to be sharpened, must be in the form of an array.
+            factor: factor by which the image gets sharpened.
+
+        Returns:
+            sharpened image array.
+        """
+        img = Image.fromarray(image)  # transform array to PIL.Image object
+        filter = ImageEnhance.Sharpness(img)  # create sharpness filter
+        sharpimg = filter.enhance(factor)  # enhance image
+        array = np.asarray(sharpimg)  # transform back to array
+        return array
+
 
 class FlirCamSetting(DeviceSetting):
     """
