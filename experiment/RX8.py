db0bf3930fbd6639ba41cc8d0442ff6a21580ee9
Max Schulz <max.schulz10@web.de>
2023-01-12 21:44:52 +0100

    Wrapped RX81 and RX82 into one handle dict

--- experiment/RX8.py
+++ experiment/RX8.py
@@ -1,7 +1,7 @@
 from labplatform.config import get_config
 from labplatform.core.Device import Device
 from labplatform.core.Setting import DeviceSetting
-from traits.api import CFloat, CInt, Str, Any, Instance
+from traits.api import CFloat, CInt, Str, Any, Instance, List
 import threading
 from labplatform.core import TDTblackbox as tdt
 import logging
@@ -11,29 +11,34 @@ import time
 log = logging.getLogger(__name__)
 
 
-class RX81Setting(DeviceSetting):  # this class contains settings for the device and sits in RX8.setting
+class RX8Setting(DeviceSetting):  # this class contains settings for the device and sits in RX8.setting
     sampling_freq = CFloat(24144.0625, group='status', dsec='Sampling frequency of the device (Hz)')
     buffer_size_max = CInt(50000, group='status', dsec='Max buffer size')
     file = Str('MSL\\RCX\\play_buf_msl.rcx', group='status', dsec='Name of the rcx file to load')
     processor = Str('RX8', group='status', dsec='Name of the processor')
     connection = Str('GB', group='status', dsec='Connection type of the processor')
-    index = CInt(1, group='status', dsec='Index of the device to connect to')
+    index = List([1, 2], group='status', dsec='Index of the device to connect to')
     signals = Any(group='primary', dsec='Stimulus to play', reinit=False)
     speakers = Any(group="primary", dsex="Speaker to pick", reinit=False)
 
 
-class RX81Device(Device):
-    setting = RX81Setting()  # device setting
+class RX8Device(Device):
+    setting = RX8Setting()  # device setting
     handle = Any()  # device handle
     thread = Instance(threading.Thread)  # important for threading
 
     def _initialize(self, **kwargs):
         expdir = get_config('DEVICE_ROOT')
         self.handle = tdt.Processors()
-        self.handle.initialize(proc_list=[f"{self.setting.processor}{self.setting.index}", self.setting.processor, self.setting.index, os.path.join(expdir, self.setting.file)],
+        self.handle.initialize(proc_list=[[f"{self.setting.processor}{self.setting.index[0]}",
+                                           self.setting.processor,
+                                          os.path.join(expdir, self.setting.file)],
+                                          [f"{self.setting.processor}{self.setting.index[1]}",
+                                           self.setting.processor,
+                                           os.path.join(expdir, self.setting.file)]],
                                connection=self.setting.connection)
         self.handle.write("playbuflen", self.setting.sampling_freq, procs=self.handle.procs)
-        print(f"Initialized {self.setting.processor}{self.setting.index}.")
+        print(f"Initialized {self.handle.procs}.")
 
         # create thread to monitoring hardware
         #if not self.thread or not self.thread.is_alive():
@@ -43,88 +48,22 @@ class RX81Device(Device):
 
     def _configure(self, **kwargs):
         for idx, spk in enumerate(self.setting.speakers):
-            self.handle.write(tag=f"data{idx}", value=self.setting.signals[idx].data.flatten(), procs=self.handle.procs)
-            self.handle.write(tag=f"chan{idx}", value=spk.channel_analog, procs=self.handle.procs)
+            self.handle.write(tag=f"data{idx}", value=self.setting.signals[idx].data.flatten(), procs=spk.TDT_analog)
+            self.handle.write(tag=f"chan{idx}", value=spk.channel_analog, procs=spk.TDT_analog)
             print(f"Set signal to chan {idx}")
-        print(f"Configured {self.setting.processor}{self.setting.index}.")
+        print(f"Configured {self.handle.procs}.")
 
     def _start(self):
-        self.handle.trigger(trig=1, proc=self.handle.procs[self.setting.processor])
-        print(f"Running {self.setting.processor}{self.setting.index} ... ")
+        self.handle.trigger(proc=self.handle.procs[self.setting.processor])
+        print(f"Running {self.handle.procs} ... ")
         self.wait_to_finish_playing()
 
     def _pause(self):
-        print(f"Pausing {self.setting.processor} ... ")
+        print(f"Pausing {self.handle.procs} ... ")
         pass
 
     def _stop(self):
-        print(f"Halting {self.handle.procs.keys()} ...")
-        self.handle.halt()
-
-    #def thread_func(self):
-        #while self.handle.read('playback', proc=f"{self.setting.processor}{self.setting.index}"):
-            #pass
-        #self.stop()
-        #self.experiment._stop_trial = True
-
-    def wait_to_finish_playing(self, proc="all", tag="playback"):
-        if proc == "all":
-            proc = list(self.handle.procs.keys())
-        elif isinstance(proc, str):
-            proc = [proc]
-        logging.info(f'Waiting for {tag} on {proc}.')
-        while any(self.handle.read(tag, proc=p) for p in proc):
-            time.sleep(0.01)
-        print('Done waiting.')
-
-class RX82Setting(DeviceSetting):  # this class contains settings for the device and sits in RX8.setting
-    sampling_freq = CFloat(24144.0625, group='status', dsec='Sampling frequency of the device (Hz)')
-    buffer_size_max = CInt(50000, group='status', dsec='Max buffer size')
-    file = Str('MSL\\RCX\\play_buf_msl.rcx', group='status', dsec='Name of the rcx file to load')
-    processor = Str('RX8', group='status', dsec='Name of the processor')
-    connection = Str('GB', group='status', dsec='Connection type of the processor')
-    index = CInt(2, group='status', dsec='Index of the device to connect to', reinit=False)
-    signals = Any(group='primary', dsec='Stimulus to play', reinit=False)
-    speakers = Any(group="primary", dsex="Speaker to pick", reinit=False)
-
-
-class RX82Device(Device):
-    setting = RX82Setting()  # device setting
-    handle = Any()  # device handle
-    thread = Instance(threading.Thread)  # important for threading
-
-    def _initialize(self, **kwargs):
-        expdir = get_config('DEVICE_ROOT')
-        self.handle = tdt.Processors()
-        self.handle.initialize(proc_list=[f"{self.setting.processor}{self.setting.index}", self.setting.processor, self.setting.index, os.path.join(expdir, self.setting.file)],
-                               connection=self.setting.connection)
-        self.handle.write("playbuflen", self.setting.sampling_freq, procs=self.handle.procs)
-        print(f"Initialized {self.setting.processor}{self.setting.index}.")
-
-        # create thread to monitoring hardware
-        #if not self.thread or not self.thread.is_alive():
-            #log.debug('creating thread...')
-            #self.thread = threading.Thread(target=self.thread_func, daemon=True)
-            #self.thread.start()
-
-    def _configure(self, **kwargs):
-        for idx, spk in enumerate(self.setting.speakers):
-            self.handle.write(tag=f"data{idx}", value=self.setting.signals[idx].data.flatten(), procs=self.handle.procs)
-            self.handle.write(tag=f"chan{idx}", value=spk.channel_analog, procs=self.handle.procs)
-            print(f"Set signal to chan {idx}")
-        print(f"Configured {self.setting.processor}{self.setting.index}.")
-
-    def _start(self):
-        self.handle.trigger(trig=1, proc=self.handle.procs[self.setting.processor])
-        print(f"Running {self.setting.processor}{self.setting.index} ... ")
-        self.wait_to_finish_playing()
-
-    def _pause(self):
-        print(f"Pausing {self.setting.processor} ... ")
-        pass
-
-    def _stop(self):
-        print(f"Halting {self.handle.procs.keys()} ...")
+        print(f"Halting {self.handle.procs} ...")
         self.handle.halt()
 
     #def thread_func(self):
