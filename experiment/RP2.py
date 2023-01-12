1756990cbd1c5b49ec6656c0c44c11a7849b04a1
Max Schulz <max.schulz10@web.de>
2023-01-12 21:25:38 +0100

    RP2 update

--- experiment/RP2.py
+++ experiment/RP2.py
@@ -17,8 +17,8 @@ class RP2Setting(DeviceSetting):  # this class contains important settings for t
     sampling_freq = CFloat(48288.125, group='status', dsec='Sampling frequency of the device (Hz)')
     buffer_size_max = CInt(50000, group='status', dsec='Max Buffer size')
     file = Str('MSL\\RCX\\button_rec.rcx', group='status', dsec='Name of the rcx file to load')
-    processor = Str('RP2', group='status', dsec='Name of the processor')
-    connection = Str('GB', group='status', dsec='Connection of the device')
+    processor = Str('RM1', group='status', dsec='Name of the processor')
+    connection = Str('USB', group='status', dsec='Connection of the device')
     index = Any(1, group='status', dsec='index of the device to connect to')
 
 
@@ -29,9 +29,8 @@ class RP2Device(Device):
 
     def _initialize(self, **kwargs):  # this method is called upon self.initialize() execution
         expdir = get_config('DEVICE_ROOT')
-        self.handle = tdt.Processors()  # set processor to handle
-        self.handle.initialize(proc_list=[self.setting.processor, self.setting.processor, self.setting.index, os.path.join(expdir, self.setting.file)],
-                               connection=self.setting.connection)  # initialize processor.
+        self.handle = tdt.initialize_processor(processor=self.setting.processor, connection=self.setting.connection,
+                                               index=self.setting.index, path=os.path.join(expdir, self.setting.file))
         print(f"Initialized {self.setting.processor}")
 
     def _configure(self, **kwargs):  # device needs to be configured before each trial. Sets state to "ready".
@@ -46,22 +45,23 @@ class RP2Device(Device):
         pass
 
     def _stop(self):  # stops the device. Initialization necessary when this method is called
-        print(f"Halting {self.handle.procs.keys()} ...")
-        self.handle.halt()
+        print(f"Halting {self.setting.processor} ...")
+        self.handle.Halt()
 
     def wait_for_button(self):  # stops the circuit as long as no button is being pressed
         print("Waiting for button press ...")
-        while not self.handle.read(tag="response", proc=self.setting.processor):
+        while not self.handle.GetTagVal("response"):
             time.sleep(0.1)  # sleeps while the response tag in the rcx circuit does not yield 1
 
     def get_response(self):  # collects response, preferably called right after wait_for_button
-        print("Acquiring response ... ")
-        response = self.handle.read("response", self.setting.processor)
+        print("Acquiring button response ... ")
+        response = self.handle.GetTagVal("response")
         return int(np.log2(response))  # because the response is stored in bit value, we need the base 2 log
 
 
 if __name__ == "__main__":
     RP2 = RP2Device()
+    RP2.initialize()
     RP2.configure()
     RP2.start()
     RP2.wait_for_button()
