843221277065a79c5a8dcf750661297bdaf2de85
Max Schulz <max.schulz10@web.de>
2023-01-13 15:59:46 +0100

    data exploration update

--- experiment/exp_data_example.py
+++ experiment/exp_data_example.py
@@ -8,7 +8,7 @@ data_root = get_config("DATA_ROOT")
 fp = os.path.join(data_root, "example\\example_control_Mouse_C1")
 
 # define filters and fields for h5Tables.extract files
-filters = (('_v_name', 'trial_log'), ('<+nyu_id', lambda id: id != 0),)
+filters = (('_v_name', 'trial_log'), ('<+nyu_id', lambda id: id != 0))
 fields = (('<+identifier', 'id'), ('<+sex', 'sex'))
 
 # get all files from cohort directory
