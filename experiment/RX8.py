64a5a5202e87c98bae17d6eeba0d7e70e0587f50
pfriedrich-hub <61830224+mxschlz@users.noreply.github.com>
2023-01-14 19:36:28 +0100

    Added name

--- experiment/RX8.py
+++ experiment/RX8.py
@@ -18,8 +18,10 @@ class RX8Setting(DeviceSetting):  # this class contains settings for the device 
     processor = Str('RX8', group='status', dsec='Name of the processor')
     connection = Str('GB', group='status', dsec='Connection type of the processor')
     index = List([1, 2], group='status', dsec='Index of the device to connect to')
-    signals = Any(group='primary', dsec='Stimulus to play', reinit=False)
+    signals = Any(group='primary', dsec='Stimulus to play', reinit=False, )
     speakers = Any(group="primary", dsex="Speaker to pick", reinit=False)
+    device_name = Str("RX8", group="status", dsec="Name of the device")
+
 
 
 class RX8Device(Device):
