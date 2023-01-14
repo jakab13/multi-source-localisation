da1c22fb0b84bff744990e32c29a247b2f732147
pfriedrich-hub <61830224+mxschlz@users.noreply.github.com>
2023-01-14 20:05:12 +0100

    some hacks and tricks

--- experiment/Numerosity_Judgement.py
+++ experiment/Numerosity_Judgement.py
@@ -15,6 +15,7 @@ import pathlib
 import time
 import numpy as np
 import logging
+import datetime
 
 log = logging.getLogger(__name__)
 
@@ -23,18 +24,17 @@ log = logging.getLogger(__name__)
 # TODO: check signal and speaker log before trial
 # TODO: Implement ArUcoCam
 # TODO: try .configure_traits method for fun
+# TODO: cannot write signals and speakers to pytables because of inhomogeneous dimensions --> store_info_before_start
 
 
 class NumerosityJudgementSetting(ExperimentSetting):
-    experiment_name = Str('Numerosity Judgement', group='status', dsec='name of the experiment', noshow=True)
-    speakers = List(group="primary", dsec="List of speakers", reinit=False)
-    signals = List(group="primary", dsec="Set to choose stimuli from", reinit=False)
-    n_blocks = CInt(1, group="status", dsec="Number of total blocks per session")
-    n_trials = CInt(20, group="status", dsec="Number of total trials per block")
+    experiment_name = Str('NumJudge', group='status', dsec='name of the experiment', noshow=True)
+    n_blocks = Int(1, group="status", dsec="Number of total blocks per session")
+    n_trials = Int(20, group="status", dsec="Number of total trials per block")
     conditions = List([2, 3, 4, 5], group="status", dsec="Number of simultaneous talkers in the experiment")
     signal_log = List([999], group="primary", dsec="Logs of the signals used in previous trials", reinit=False)
     speaker_log = List([999], group="primary", dsec="Logs of the speakers used in previous trials", reinit=False)
-    trial_duration = Float(1.0, group="status", dsec="Duration of one trial")
+    # trial_duration = Float(1.0, group="status", dsec="Duration of one trial")
 
 
 class NumerosityJudgementExperiment(ExperimentLogic):
@@ -47,14 +47,16 @@ class NumerosityJudgementExperiment(ExperimentLogic):
     signals_sample = List()
     response = Int()
     reaction_time = Int()
-    time_0 = time.time()
+    time_0 = Float()
+    speakers = List()
+    signals = List()
 
     def _initialize(self, **kwargs):
+        self.load_speakers()
+        self.load_signals()
         self.devices["RP2"] = RP2Device()
         self.devices["RX8"] = RX8Device()
         # self.devices["ArUcoCam"] = ArUcoCam()
-        self.load_speakers()
-        self.load_signals()
 
     def _start(self, **kwargs):
         pass
@@ -65,13 +67,17 @@ class NumerosityJudgementExperiment(ExperimentLogic):
     def _stop(self, **kwargs):
         pass
 
+    def _before_start_validate(self):
+        for device in self.devices.keys():
+            if self.devices[device].state != "Ready":
+                self.devices[device].change_state("Ready")
+
     def _start_trial(self):
-        start_time = time.time()
-        self.devices["RX8"].configure()
+        self.time_0 = time.time()
         self.devices["RX8"].start()
-        self.devices["RP2"].wait_for_button()
-        self.response = self.devices["RP2"].get_response()
-        self.reaction_time = int(round(time.time() - start_time, 3) * 1000)
+        # self.devices["RP2"].wait_for_button()
+        # self.response = self.devices["RP2"].get_response()
+        self.reaction_time = int(round(time.time() - self.time_0, 3) * 1000)
         log.info('trial {} start: {}'.format(self.setting.current_trial, time.time() - self.time_0))
 
     def _stop_trial(self):
@@ -82,45 +88,42 @@ class NumerosityJudgementExperiment(ExperimentLogic):
         self.data.write(key="reaction_time", data=self.reaction_time, current_trial=current_trial)
         self.data.write(key="is_correct", data=is_correct, current_trial=current_trial)
         self.data.save()
-        log.info('trial {} start: {}'.format(self.setting.current_trial, time.time() - self.time_0))
+        log.info('trial {} start: {}'.format(current_trial, time.time() - self.time_0))
         self.time_0 = time.time()
 
     def setup_experiment(self, info=None):
         # trial_length = self.devices['RX8Device'].setting.sampling_freq * self.setting.trial_duration
-        self.devices['RX8Device'].configure()
         self.sequence.__next__()
         self.pick_speakers_this_trial(n_speakers=self.sequence.this_trial)
         self.pick_signals_this_trial(n_signals=self.sequence.this_trial)
+        self.devices["RX8"].configure()
         print("Set up experiment!")
-
-    # def start_experiment(self, info=None):
-        # pass
 
     def load_signals(self, sound_type="tts-countries_resamp_24414"):
         sound_root = get_config(setting="SOUND_ROOT")
         sound_fp = pathlib.Path(os.path.join(sound_root, sound_type))
         sound_list = slab.Precomputed(slab.Sound.read(pathlib.Path(sound_fp / file)) for file in os.listdir(sound_fp))
-        self.setting.signals = sound_list
+        self.signals = sound_list
 
     def load_speakers(self, filename="dome_speakers.txt"):
         basedir = get_config(setting="BASE_DIRECTORY")
         filepath = os.path.join(basedir, filename)
         spk_array = SpeakerArray(file=filepath)
         spk_array.load_speaker_table()
-        speakers = spk_array.pick_speakers([x for x in range(19, 28)])
-        self.setting.speakers = speakers
+        speakers = spk_array.pick_speakers([x for x in range(20, 27)])
+        self.speakers = speakers
 
     @staticmethod
     def set_log():
         pass
 
     def pick_speakers_this_trial(self, n_speakers):
-        speakers_no_rep = list(x for x in self.setting.speakers if x not in self.setting.speaker_log)
-        self.speakers_sample = random.sample(speakers_no_rep, n_speakers)
+        speakers_no_rep = list(x for x in self.speakers if x not in self.setting.speaker_log)
+        self.devices["RX8"].setting.speakers = random.sample(speakers_no_rep, n_speakers)
 
     def pick_signals_this_trial(self, n_signals):
-        signals_no_rep = list(x for x in self.setting.signals if x not in self.setting.signal_log)
-        self.signals_sample = random.sample(signals_no_rep, n_signals)
+        signals_no_rep = list(x for x in self.signals if x not in self.setting.signal_log)
+        self.devices["RX8"].setting.signals = random.sample(signals_no_rep, n_signals)
 
     def calibrate_camera(self, report=True, limit=0.5):
         """
@@ -167,24 +170,18 @@ class NumerosityJudgementExperiment(ExperimentLogic):
 
 
 if __name__ == "__main__":
-    try:
-        subject = Subject()
-        subject.name = "Max"
-        subject.group = "Test"
-        subject.species = "Human"
-        subject.data_path = os.path.join(get_config("DATA_ROOT"), subject.name)
-        subject.add_subject_to_h5file(os.path.join(get_config("SUBJECT_ROOT"), "Max_Test.h5"))
-        # subject.file_path
-    except ValueError:
-        # read the subject information
-        sl = SubjectList(file_path=os.path.join(get_config("SUBJECT_ROOT"), "Max_Test.h5"))
-        sl.read_from_h5file()
-        test_subject = sl.subjects[0]
+    subject = Subject()
+    subject.name = "Max"
+    subject.group = "Test"
+    subject.species = "Human"
+    subject.birth = datetime.date(1996, 11, 18)
+    subject.data_path = os.path.join(get_config("DATA_ROOT"), subject.name)
+    subject.add_subject_to_h5file(os.path.join(get_config("SUBJECT_ROOT"), "Max_Test.h5"))
+    # subject.file_path
     nj = NumerosityJudgementExperiment(subject=subject)
-    nj.configure_traits()
-
+    # nj.initialize()
+    # nj.configure()
     nj.start()
-    nj.pause()
-    nj.stop()
+    nj.change_state("Paused")
 
-
+    # nj.configure_traits()
