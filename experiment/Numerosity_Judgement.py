2eca34e919c1c922f76d71ebbcce802a617e2fa6
Max Schulz <max.schulz10@web.de>
2023-01-14 22:34:41 +0100

    added logger

--- experiment/Numerosity_Judgement.py
+++ experiment/Numerosity_Judgement.py
@@ -183,6 +183,17 @@ class NumerosityJudgementExperiment(ExperimentLogic):
 
 
 if __name__ == "__main__":
+    log = logging.getLogger()
+    log.setLevel(logging.DEBUG)
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # add formatter to ch
+    ch.setFormatter(formatter)
+    # add ch to logger
+    log.addHandler(ch)
     subject = Subject()
     subject.name = "Foo"
     subject.group = "Test"
