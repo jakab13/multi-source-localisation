9403c8e55f90fce573afe4c0ddac770bfe97ded3
pfriedrich-hub <61830224+mxschlz@users.noreply.github.com>
2023-02-02 19:23:26 +0100

    small script for experiment execution

---
+++ experiment/experiment_run.py
@@ -0,0 +1,13 @@
+from experiment.utils import setup_experiment, run_experiment
+
+
+# STEP 1: set up experiment settings
+exp = setup_experiment()
+
+# STEP 2: calibrate camera
+exp.calibrate_camera()
+
+# STEP 3: start experiment
+run_experiment(experiment=exp, n_blocks=5)
+
+
