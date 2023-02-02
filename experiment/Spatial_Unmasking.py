3c8bd13e89ebd1196d5addb373bc6f846e0d1c62
pfriedrich-hub <61830224+mxschlz@users.noreply.github.com>
2023-02-02 19:25:12 +0100

    got rid of solution converter

--- experiment/Spatial_Unmasking.py
+++ experiment/Spatial_Unmasking.py
@@ -21,7 +21,6 @@ log = logging.getLogger(__name__)
 
 # TODO: check out threading module
 # TODO: data saving still sucks! log_trial() maybe? set_h5_atrributes()? data.data_spec?
-# TODO: why does the camera calibration work on NumJudge but not here??
 # TODO: clear camera buffer every once in a while
 
 class SpatialUnmaskingSetting(ExperimentSetting):
@@ -75,22 +74,19 @@ class SpatialUnmaskingExperiment(ExperimentLogic):
         self.masker_speaker = Any()
         self.selected_target_sounds = self.signals[talker * 5:(talker + 1) * 5]  # select numbers 1-5 for one talker
         self._tosave_para["sequence"] = self.sequence
+        self.devices["RX8"].handle.write(tag='bitmask',
+                                         value=1,
+                                         procs="RX81")  # illuminate central speaker LED
 
     def _prepare_trial(self):
         self.stairs = slab.Staircase(start_val=70, n_reversals=2, step_sizes=[4, 1])  # renew
         self.sequence.__next__()
         self.masker_speaker = self.speakers[self.sequence.this_n]
 
-    solution_converter = {
-        1: 5,
-        2: 4,
-        3: 1,
-        4: 3,
-        5: 2
-    }
-
     def _start_trial(self):
+        self.time_0 = time.time()  # starting time of the trial
         for level in self.stairs:
+            log.warning(f"trial {self.setting.current_trial} dB level: {level}")
             self.check_headpose()
             target_sound_i = random.choice(range(len(self.selected_target_sounds)))
             target_sound = self.selected_target_sounds[target_sound_i]  # choose random number from sound_list
@@ -107,17 +103,16 @@ class SpatialUnmaskingExperiment(ExperimentLogic):
             self.devices["RX8"].handle.write("data1",
                                              self.masker_sound.data.flatten(),
                                              f"{self.masker_speaker.TDT_analog}{self.masker_speaker.TDT_idx_analog}")
-            self.time_0 = time.time()  # starting time of the trial
-            log.info('trial {} start: {}'.format(self.setting.current_trial, time.time() - self.time_0))
+            log.warning('trial {} start: {}'.format(self.setting.current_trial, time.time() - self.time_0))
             # simulate response
-            response = self.stairs.simulate_response(threshold=3)
+            response = self.stairs.simulate_response(threshold=60)
             self.stairs.add_response(response)
             self.devices["RX8"].start()
             self.devices["RX8"].pause()
             # self.devices["RP2"].wait_for_button()
             reaction_time = int(round(time.time() - self.time_0, 3) * 1000)
             # response = self.devices["RP2"].get_response()
-            solution = self.solution_converter[target_sound_i + 1]
+            solution = target_sound_i + 1
             is_correct = True if solution / response == 1 else False
             # self.stairs.add_response(1) if response/solution is True else self.stairs.add_response(0)
             self.stairs.plot()
@@ -127,17 +122,16 @@ class SpatialUnmaskingExperiment(ExperimentLogic):
                                    is_correct=is_correct)
             self.data.save()
         self.stairs.close_plot()
-        self.process_event({'trial_stop': 0})
+        self.process_event({'trial_stop': 0})  # stops the trial
 
     def _stop_trial(self):
-        self.data.set_h5_attrs(threshold=self.stairs.threshold())
+        #self.data.set_h5_attrs(threshold=self.stairs.threshold())
         #is_correct = True if self.sequence.this_trial / self.devices["RP2"]._output_specs["response"] == 1 else False
-
         #self.data.write(key="solution", data=self.sequence.this_trial)
         #self.data.write(key="reaction_time", data=self.reaction_time)
         #self.data.write(key="is_correct", data=is_correct)
-        self.data.save()
-        log.info('trial {} end: {}'.format(self.setting.current_trial, time.time() - self.time_0))
+        #self.data.save()
+        log.warning('trial {} end: {}'.format(self.setting.current_trial, time.time() - self.time_0))
 
     def load_signals(self, sound_type="tts-numbers_resamp_24414"):
         sound_root = get_config(setting="SOUND_ROOT")
@@ -159,24 +153,28 @@ class SpatialUnmaskingExperiment(ExperimentLogic):
         Calibrates the cameras. Initializes the RX81 to access the central loudspeaker. Illuminates the led on ele,
         azi 0°, then acquires the headpose and uses it as the offset. Turns the led off afterwards.
         """
-        log.info("Calibrating camera")
-        led = self.speakers[3]  # central speaker
+        log.warning("Calibrating camera")
+        led = self.target_speaker  # central speaker
         self.devices["RX8"].handle.write(tag='bitmask',
                                          value=1,
                                          procs="RX81")  # illuminate central speaker LED
-        log.info('Point towards led and press button to start calibration')
+        log.warning('Point towards led and press button to start calibration')
         self.devices["RP2"].wait_for_button()  # start calibration after button press
         self.devices["ArUcoCam"].start()
         offset = self.devices["ArUcoCam"].get_pose()
         self.devices["ArUcoCam"].offset = offset
         self.devices["ArUcoCam"].pause()
+        for i, v in enumerate(self.devices["ArUcoCam"].offset):  # check for NoneType in offset
+            if v is None:
+                self.devices["ArUcoCam"].offset[i] = 0
+                log.warning("Calibration unsuccessful, make sure markers can be detected by cameras!")
         self.devices["RX8"].handle.write(tag='bitmask',
                                          value=0,
                                          procs=f"{led.TDT_digital}{led.TDT_idx_digital}")  # turn off LED
         self.devices["ArUcoCam"].calibrated = True
         if report:
-            log.info(f"Camera offset: {offset}")
-        log.info('Calibration complete!')
+            log.warning(f"Camera offset: {offset}")
+        log.warning('Calibration complete!')
 
     def check_headpose(self):
         while True:
@@ -185,7 +183,7 @@ class SpatialUnmaskingExperiment(ExperimentLogic):
             self.devices["ArUcoCam"].pause()
             try:
                 if np.sqrt(np.mean(np.array(self.devices["ArUcoCam"].setting.pose) ** 2)) > 10:
-                    log.info("Subject is not looking straight ahead")
+                    log.warning("Subject is not looking straight ahead")
                     for idx in range(1, 5):  # clear all speakers before loading warning tone
                         self.devices["RX8"].handle.write(f"data{idx}", 0, procs=["RX81", "RX82"])
                         self.devices["RX8"].handle.write(f"chan{idx}", 99, procs=["RX81", "RX82"])
@@ -196,16 +194,17 @@ class SpatialUnmaskingExperiment(ExperimentLogic):
                 else:
                     break
             except TypeError:
-                log.info("Cannot detect markers, make sure cameras are set up correctly and arucomarkers can be detected.")
+                log.warning("Cannot detect markers, make sure cameras are set up correctly and arucomarkers can be detected.")
                 continue
+
 
 if __name__ == "__main__":
 
     log = logging.getLogger()
-    log.setLevel(logging.INFO)
+    log.setLevel(logging.WARNING)
     # create console handler and set level to debug
     ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
+    ch.setLevel(logging.WARNING)
     # create formatter
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     # add formatter to ch
@@ -233,6 +232,6 @@ if __name__ == "__main__":
     # subject.file_path
     experimenter = "Max"
     su = SpatialUnmaskingExperiment(subject=subject, experimenter=experimenter)
-    # su.calibrate_camera()
-    # su.start()
+    su.calibrate_camera()
+    su.start()
     # su.configure_traits()
