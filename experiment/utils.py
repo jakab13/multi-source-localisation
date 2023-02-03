5d57399fb7908f8c61763fa87162dd93cfb3c583
pfriedrich-hub <61830224+mxschlz@users.noreply.github.com>
2023-02-03 19:14:19 +0100

    changed subject naming

--- experiment/utils.py
+++ experiment/utils.py
@@ -21,27 +21,25 @@ log.addHandler(ch)
 
 def setup_experiment():
     exp_type = input("su or nm?").lower()
-    subject = input("Enter subject name: ").lower()
+    subject = input("Enter subject id: ").lower()
     group = input("Vertical or horizontal? Insert v or h").lower()
     sex = input("m or f? ").upper()
     cohort = input("Pilot or Test cohort? Insert p or t").lower()
     experimenter = input("Enter your name: ").lower()
-
     try:
-        subject = Subject(name=subject,
+        subject = Subject(name=f"sub{subject}",
                           group=group,
                           species="Human",
                           sex=sex,
                           cohort=cohort)
-        subject.data_path = os.path.join(get_config("DATA_ROOT"), f"{subject}.h5")
-        subject.add_subject_to_h5file(os.path.join(get_config("SUBJECT_ROOT"), f"{subject}.h5"))
-        # test_subject.file_path
+        subject.data_path = os.path.join(get_config("DATA_ROOT"), f"sub{subject}.h5")
+        subject.add_subject_to_h5file(os.path.join(get_config("SUBJECT_ROOT"), f"sub{subject}.h5"))
     except ValueError:
         # read the subject information
-        sl = SubjectList(file_path=os.path.join(get_config("SUBJECT_ROOT"), f"{subject}.h5"))
+        sl = SubjectList(file_path=os.path.join(get_config("SUBJECT_ROOT"), f"sub{subject}.h5"))
         sl.read_from_h5file()
         subject = sl.subjects[0]
-        subject.data_path = os.path.join(get_config("DATA_ROOT"), f"{subject}.h5")
+        subject.data_path = os.path.join(get_config("DATA_ROOT"), f"sub{subject}.h5")
 
     if exp_type == "su":
         experiment = SpatialUnmaskingExperiment(subject=subject, experimenter=experimenter)
@@ -56,5 +54,5 @@ def setup_experiment():
 def run_experiment(experiment, n_blocks):
     for _ in range(n_blocks):
         experiment.start()
-        input("Press any key to continue experiment")
-        experiment.reset()
+        if experiment.state == "Stopped":
+            input("Press any key to continue experiment")
