f5eda517117cdb348bae5c8f592064a491f99703
Max Schulz <max.schulz10@web.de>
2023-03-13 13:27:39 +0100

    added functions for numjudge plotting

--- analysis/utils/plotting.py
+++ analysis/utils/plotting.py
@@ -8,7 +8,7 @@ def mosaic_plot(layout):
     return fig, ax
 
 
-def draw_lineplot_actual_vs_perceived(data, sub_ids, plane, ax, errorbar=("se", 1)):
+def draw_lineplot_actual_vs_perceived(data, sub_ids, plane, ax=None, errorbar=("se", 1), **kwargs):
     for sub_id in sub_ids:
         sub = data.loc[sub_id].reset_index()
         if plane == "horizontal":
@@ -19,9 +19,117 @@ def draw_lineplot_actual_vs_perceived(data, sub_ids, plane, ax, errorbar=("se", 
             perceived = get_elevation_from_df(sub.perceived)
         else:
             raise ValueError("Plane parameter must be vertical or horizontal")
-        sns.lineplot(x=actual,
-                     y=perceived,
-                     err_style="bars",
-                     errorbar=errorbar,
-                     label=sub_id,
-                     ax=ax)
+        if ax:
+            sns.lineplot(x=actual,
+                         y=perceived,
+                         err_style="bars",
+                         errorbar=errorbar,
+                         label=sub_id,
+                         ax=ax,
+                         **kwargs)
+        else:
+            sns.lineplot(x=actual,
+                         y=perceived,
+                         err_style="bars",
+                         errorbar=errorbar,
+                         label=sub_id,
+                         **kwargs)
+
+
+def draw_lineplot_solution_vs_response(data, sub_ids, ax=None, errorbar=("se", 1), **kwargs):
+    for sub_id in sub_ids:
+        sub = data.loc[sub_id].reset_index()
+        if ax:
+            sns.lineplot(x=sub.solution,
+                         y=sub.response,
+                         err_style="bars",
+                         errorbar=errorbar,
+                         label=sub_id,
+                         ax=ax,
+                         **kwargs)
+        else:
+            sns.lineplot(x=sub.solution,
+                         y=sub.response,
+                         err_style="bars",
+                         errorbar=errorbar,
+                         label=sub_id,
+                         **kwargs)
+
+
+def draw_boxplot_actual_vs_perceived(data, sub_ids, plane, ax=None, **kwargs):
+    for sub_id in sub_ids:
+        sub = data.loc[sub_id].reset_index()
+        if plane == "horizontal":
+            actual = get_azimuth_from_df(sub.actual)
+            perceived = get_azimuth_from_df(sub.perceived)
+        elif plane == "vertical":
+            actual = get_elevation_from_df(sub.actual)
+            perceived = get_elevation_from_df(sub.perceived)
+        else:
+            raise ValueError("Plane parameter must be vertical or horizontal")
+        actual = replace_in_array(actual)
+        perceived = replace_in_array(perceived)
+        if ax:
+            sns.boxplot(x=actual,
+                        y=perceived,
+                        ax=ax,
+                        **kwargs)
+        else:
+            sns.boxplot(x=actual,
+                        y=perceived,
+                        **kwargs)
+
+
+def draw_linear_regression_actual_vs_perceived(data, plane, ax=None, axlim=(-75, 75), **kwargs):
+    if plane == "horizontal":
+        x = get_azimuth_from_df(data.actual)
+        y = get_azimuth_from_df(data.perceived)
+    elif plane == "vertical":
+        x = get_elevation_from_df(data.actual)
+        y = get_elevation_from_df(data.perceived)
+    elif plane == "all":
+        x = list()
+        y = list()
+        x.append(get_elevation_from_df(data.actual))
+        y.append(get_elevation_from_df(data.perceived))
+        x.append(get_azimuth_from_df(data.actual))
+        y.append(get_azimuth_from_df(data.perceived))
+        xflat = [item for sublist in x for item in sublist]
+        yflat = [item for sublist in y for item in sublist]
+    else:
+        raise ValueError("Plane parameter must be vertical or horizontal")
+    if plane == "all":
+        xflat = replace_in_array(xflat)
+        yflat = replace_in_array(yflat)
+        if ax:
+            sns.regplot(x=xflat,
+                        y=yflat,
+                        ax=ax,
+                        **kwargs)
+            ax.set_ylim(axlim)
+        else:
+            sns.regplot(x=xflat,
+                        y=yflat,
+                        ax=ax,
+                        **kwargs)
+    else:
+        x = replace_in_array(x)
+        y = replace_in_array(y)
+        if ax:
+            sns.regplot(x=x,
+                        y=y,
+                        ax=ax,
+                        **kwargs)
+            ax.set_ylim(axlim)
+        else:
+            sns.regplot(x=x,
+                        y=y,
+                        ax=ax,
+                        **kwargs)
+
+
+def replace_in_array(x, to_replace_val=None, replace_with_val=0):
+    for i, val in enumerate(x):
+        if val == to_replace_val:
+            x[i] = replace_with_val
+    return x
