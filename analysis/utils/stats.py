9d4e4e37345345443c8255b98075a0a8fae5ea36
Max Schulz <max.schulz10@web.de>
2023-03-21 15:13:16 +0100

    added possible combinations

--- analysis/utils/stats.py
+++ analysis/utils/stats.py
@@ -1,4 +1,5 @@
 import math
+import sklearn
 from sklearn.linear_model import LinearRegression
 
 
@@ -6,8 +7,19 @@ def gain(x, y):
     return LinearRegression().fit(x, y)
 
 
+def possible_combinations(n, k):
+    return math.comb(n, k)
+
+
 if __name__ == "__main__":
-    n = 7
-    k = 5
-    combinations = math.comb(n, k)
-    print(combinations)
+    from analysis.utils.misc import load_dataframe
+    from labplatform.config import get_config
+    import os
+    import pandas as pd
+    import seaborn as sn
+    dd = os.path.join(get_config("DATA_ROOT"), "MSL")
+    df = load_dataframe(data_dir=dd, exp_name="NumJudge", plane="h")
+    cm = pd.crosstab(df['response'], df['solution'], rownames=['response'], colnames=['solution'], margins=True,
+                     normalize=True)
+    cm = cm.drop(index=8)
+    sn.heatmap(cm, annot=True)
