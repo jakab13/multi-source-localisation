73e90bebfffa517cc523e72822be8d63879a1046
Max Schulz <max.schulz10@web.de>
2023-07-17 16:50:01 +0200

    control reversed sounds

--- temp/control_spectral_coverage.py
+++ temp/control_spectral_coverage.py
@@ -27,7 +27,6 @@ clearspeech_v = dfv[np.where(filled_v==False, True, False)]  # True where revers
 signals_sample_reversed_h = revspeech_h.signals_sample  # talker IDs
 country_idxs_reversed_h = revspeech_h.country_idxs  # indices of the country names from a talker
 
-
 # get sub ids
 sub_ids = extract_subject_ids_from_dataframe(dfh)
 
@@ -36,9 +35,14 @@ talker_files_path = os.path.join(get_config("SOUND_ROOT"), "numjudge_talker_file
 with open(talker_files_path, "rb") as files:
     sounds_clear = pkl.load(files)
 
-talker_files_path = os.path.join(get_config("SOUND_ROOT"), "numjudge_talker_files_reversed.pkl")
-with open(talker_files_path, "rb") as files:
-    sounds_reversed = pkl.load(files)
+talkers = list(sounds_clear.keys())
+
+sounds_reversed = dict()
+for talker in talkers:
+    sounds_reversed[talker] = list()
+    for sound in sounds_clear[talker]:
+        revsound = slab.Sound(data=sound[::-1], samplerate=sound.samplerate)
+        sounds_reversed[talker].append(revsound)
 
 dyn_range = 65  # threshold dB yielding highest coverage variance
 upper_freq = 11000
@@ -48,7 +52,7 @@ for trial_n in range(revspeech_h.__len__()):
     # reversed
     signals = signals_sample_reversed_h[trial_n]
     country_idx = country_idxs_reversed_h[trial_n]
-    trial_composition = [sounds_reversed[x][y].resize(1.0) for x, y in zip(signals, country_idx)]
+    trial_composition = [sounds_reversed[x][y].resize(0.6) for x, y in zip(signals, country_idx)]
     percentage_filled = spectemp_coverage(trial_composition, dyn_range, upper_freq=upper_freq)
     revspeech_data_h["sound"].append(sum(trial_composition))
     revspeech_data_h["coverage"].append(percentage_filled)
@@ -62,5 +66,4 @@ sns.lineplot(x=revspeech_h.solution, y=coverage.loc["revspeech_h"]["coverage"], 
 plt.title("Spectral Density Horizontal")
 plt.xlabel("Sound source perceptional discrepancy")
 plt.ylabel("coverage")
-
 
