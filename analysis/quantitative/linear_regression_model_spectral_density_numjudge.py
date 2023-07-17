f8e7128374e3bec0c38e3beb58da8b2e6a19b58a
Max Schulz <max.schulz10@web.de>
2023-07-17 16:50:27 +0200

    first attempt

---
+++ analysis/quantitative/linear_regression_model_spectral_density_numjudge.py
@@ -0,0 +1,46 @@
+from analysis.utils.plotting import *
+import os
+import seaborn as sns
+import pickle as pkl
+from labplatform.config import get_config
+sns.set_theme()
+from sklearn.linear_model import LinearRegression
+
+
+# load data from all subjects
+fp = os.path.join(get_config("DATA_ROOT"), "MSL")
+exp_name = "NumJudge"
+dfv = load_dataframe(fp, exp_name=exp_name, plane="v")
+dfh = load_dataframe(fp, exp_name=exp_name, plane="h")
+
+filled_h = dfh.reversed_speech.ffill()
+revspeech_h = dfh[np.where(filled_h==True, True, False)]  # True where reversed_speech is True
+clearspeech_h = dfh[np.where(filled_h==False, True, False)]  # True where reversed_speech is False
+
+# vertical
+filled_v = dfv.reversed_speech.ffill()
+revspeech_v = dfv[np.where(filled_v==True, True, False)]  # True where reversed_speech is True
+clearspeech_v = dfv[np.where(filled_v==False, True, False)]  # True where reversed_speech is False
+
+# get sub ids
+sub_ids = extract_subject_ids_from_dataframe(dfh)
+
+# get covariate
+coverage = pkl.load(open("Results/coverage_dataframe.pkl", "rb"))
+
+# prepare dataframe for the model
+df = pd.DataFrame(columns=["solution", "response", "coverage"])
+df.solution = clearspeech_v.solution
+df.response = clearspeech_v.response
+df.coverage = coverage.loc["clearspeech_v"]["coverage"]
+df = df.fillna(0)
+
+# model
+x = df.solution.values.reshape(-1, 1)
+y = df.coverage.values.reshape(-1, 1)
+model = LinearRegression(fit_intercept=True)
+model.fit(x, y)
+ypred = model.predict(x)
+
+plt.scatter(x, y)
+plt.plot(x, ypred)
