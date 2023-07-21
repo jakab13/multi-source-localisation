11be152aab3d35e6804283cadc22b0273a44d3bd
Max Schulz <max.schulz10@web.de>
2023-07-21 09:06:44 +0200

    Add regressor params

--- analysis/quantitative/ANCOVA_spectral_density_numjudge.py
+++ analysis/quantitative/ANCOVA_spectral_density_numjudge.py
@@ -15,26 +15,38 @@ dfh = load_dataframe(fp, exp_name=exp_name, plane="h")
 
 filled_h = dfh.reversed_speech.ffill()
 revspeech_h = dfh[np.where(filled_h==True, True, False)]  # True where reversed_speech is True
+revspeech_h = revspeech_h.sort_index()
 clearspeech_h = dfh[np.where(filled_h==False, True, False)]  # True where reversed_speech is False
+clearspeech_h = clearspeech_h.sort_index()
 
 # vertical
 filled_v = dfv.reversed_speech.ffill()
 revspeech_v = dfv[np.where(filled_v==True, True, False)]  # True where reversed_speech is True
+revspeech_v = revspeech_v.sort_index()
 clearspeech_v = dfv[np.where(filled_v==False, True, False)]  # True where reversed_speech is False
-
-# get sub ids
-sub_ids = extract_subject_ids_from_dataframe(dfh)
+clearspeech_v = clearspeech_v.sort_index()
 
 # get covariate
 coverage = pkl.load(open("Results/coverage_dataframe.pkl", "rb"))
 
 # prepare dataframe for the model
-df = pd.DataFrame(columns=["solution", "response", "coverage"])
-df.solution = clearspeech_v.solution
-df.response = clearspeech_v.response
-df.coverage = coverage.loc["clearspeech_v"]["coverage"]
+df = pd.DataFrame(columns=["solution", "response", "coverage", "performance"])
+df.solution = clearspeech_h.solution
+df.response = clearspeech_h.response
+df.coverage = coverage.loc["clearspeech_h"]["coverage"]
+performance = pd.read_csv("/home/max/labplatform/data/csv/numjudge_performance_clearspeech_h.csv", index_col=1)
+performance.pop("Sub_ID")
+performance = [x[0] for x in performance.values.tolist()]
+df.performance = performance
 df = df.fillna(0)
 
-# ANCOVA
+
+"""
+The interesting fact is that the ANCOVA is literally the same procedure as doing a linear regression:
+response = Beta0 + Beta1 x solution + Beta2 x coverage
+The corresponding values for the coefficients can be taken from the ANOVA table.
+"""
+
+# linear model
 model = ols('response ~ solution + coverage', data=df).fit()
 model.summary()
