ff6794804821c77b87f29eee100330f6b341e800
Max Schulz <max.schulz10@web.de>
2023-07-21 15:16:54 +0200

    nested function process_subset

--- analysis/utils/stats.py
+++ analysis/utils/stats.py
@@ -1,31 +1,39 @@
 from statsmodels.regression.linear_model import OLS
-import time
 import itertools
 import pandas as pd
 
 
-def process_subset(feature_set, X, y):
-    # Fit model on feature_set and calculate RSS
-    model = OLS(y, X[list(feature_set)])
-    regr = model.fit()
-    RSS = ((regr.predict(X[list(feature_set)]) - y) ** 2).sum()
-    return {"model": regr, "RSS": RSS}
-
-
 def mallows_ck(k, X, y):
-    tic = time.time()
     results = []
+
+    def process_subset(feature_set, X, y):
+        # Fit model on feature_set and calculate RSS
+        model = OLS(y, X[list(feature_set)])
+        regr = model.fit()
+        RSS = ((regr.predict(X[list(feature_set)]) - y) ** 2).sum()
+        return {"model": regr, "RSS": RSS}
+
     for combo in itertools.combinations(X.columns, k):
         results.append(process_subset(combo, X, y))
     # Wrap everything up in a nice dataframe
     models = pd.DataFrame(results)
     # Choose the model with the highest RSS
     best_model = models.loc[models['RSS'].argmin()]
-    toc = time.time()
-    print("Processed", models.shape[0], "models on", k, "predictors in", (toc - tic), "seconds.")
+    print("Processed", models.shape[0], "models on", k, "predictors")
     # Return the best model, along with some other useful information about the model
     return best_model
 
 
 if __name__ == "__main__":
-    pass
+    fromfp = "/home/max/labplatform/data/csv/final_df_general.csv"
+    df = pd.read_csv(fromfp, index_col=1)
+    df.pop("Sub_ID")
+    y = df.performance
+    df.pop("performance")
+    x = df
+    # Could take quite awhile to complete...
+    models_best = pd.DataFrame(columns=["RSS", "model"])
+    for k in range(1, (len(x.columns))):
+        models_best.loc[k] = mallows_ck(k, x, y)
+    print(models_best.loc[3, "model"].summary())
+    print(mallows_ck(3, x, y)["model"].summary())
