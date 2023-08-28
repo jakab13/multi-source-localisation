892ddc3cae32bf361ad8f7556976e48ca0c11e1b
Max Schulz <max.schulz10@web.de>
2023-08-28 15:35:24 +0200

    save spatmask performance slopes

--- analysis/visualization/SpatMask/get_spatmask_slopes.py
+++ analysis/visualization/SpatMask/get_spatmask_slopes.py
@@ -2,10 +2,11 @@ import pickle
 import os
 from sklearn.linear_model import LinearRegression
 import numpy as np
+import pandas as pd
 
 root = "/home/max/labplatform/data/DataFrames/"
-su_fph = os.path.join(root, "spatmask_abs_distance_h")
-su_fpv = os.path.join(root, "spatmask_abs_distance_v")
+su_fph = os.path.join(root, "spatmask_abs_distance_h.pkl")
+su_fpv = os.path.join(root, "spatmask_abs_distance_v.pkl")
 la_fph = os.path.join(root, "locaaccu_dfh")
 la_fpv = os.path.join(root, "locaaccu_dfv")
 
@@ -20,7 +21,7 @@ model = LinearRegression(fit_intercept=True)  # linear regression model
 single_subs = dict()
 for col in suh.columns:
     single_subs[col] = list()
-    for thresh1, thresh2 in zip(suh[col][:14], suh[col][14:]):
+    for thresh1, thresh2 in zip(suh[col][:13], suh[col][13:]):
         single_subs[col].append((thresh1 + thresh2) / 2)
 
 slopes = list()
@@ -31,3 +32,10 @@ for y in Y:
     model.fit(X, y)
     slopes.append(model.coef_)
 
+# save slopes and processed spatmask data
+csvpath = "/home/max/labplatform/data/csv"
+df = pd.DataFrame(single_subs)
+df.to_csv(os.path.join(csvpath, "spatmask_h_thresholds.csv"))
+
+df = pd.DataFrame(slopes, columns=["slope"])
+df.to_csv(os.path.join(csvpath, "spatmask_h_linear_slopes.csv"))
